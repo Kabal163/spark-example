af4f39622aac2faa4dd3092f1dd86a3b9667b595
Dmitrii_Kanaev <Dmitrii_Kanaev@epam.com>
2019-03-12 11:02:20 +0400

    init commit

---
+++ build.sbt
@@ -0,0 +1,12 @@
+name := "spark-scala"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+
+val sparkVersion = "2.2.1"
+
+libraryDependencies ++= Seq (
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
---
+++ src/main/scala/com/epam/hadoopcourse/homework/spark/HelloSpark.scala
@@ -0,0 +1,52 @@
+package com.epam.hadoopcourse.homework.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+object HelloSpark {
+
+  def main(args: Array[String]): Unit = {
+
+    val conf: SparkConf = new SparkConf()
+      .setAppName("HelloSpark")
+
+    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
+    import spark.implicits._
+
+    val ds: Dataset[TrainModel] = spark.read.parquet(args(0)).as[TrainModel]
+
+    val result = ds
+      .filter(_.srch_adults_cnt.get == 2)
+      .groupByKey(train => (train.hotel_continent, train.hotel_country, train.hotel_market))
+      .count()
+      .sort($"count(1)".desc)
+      .take(3)
+
+    spark.createDataset(result).write.json("/user/root/task1.json")
+  }
+
+  case class TrainModel(date_time: String,
+                        site_name: Int,
+                        posa_continent: Int,
+                        user_location_country: Int,
+                        user_location_region: Int,
+                        user_location_city: Int,
+                        orig_destination_distance: Option[Double],
+                        user_id: Long,
+                        is_mobile: Option[Boolean],
+                        is_package: Option[Boolean],
+                        channel: Option[Int],
+                        srch_ci: Option[String],
+                        srch_co: Option[String],
+                        srch_adults_cnt: Option[Int],
+                        srch_children_cnt: Option[Int],
+                        srch_rm_cnt: Option[Int],
+                        srch_destination_id: Option[Long],
+                        srch_destination_type_id: Option[Long],
+                        is_booking: Option[Boolean],
+                        cnt: Option[Int],
+                        hotel_continent: Option[Int],
+                        hotel_country: Option[Int],
+                        hotel_market: Option[Int],
+                        hotel_cluster: Option[Int])
+}
