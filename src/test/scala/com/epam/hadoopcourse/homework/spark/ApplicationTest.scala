6e6f793790cc69de59b5ccbac166eefc8c825e18
Dmitrii_Kanaev <Dmitrii_Kanaev@epam.com>
2019-03-13 10:42:10 +0400

    Removed unnecessary spark context from text and remove lazy initialization of SparkSession

--- src/test/scala/com/epam/hadoopcourse/homework/spark/ApplicationTest.scala
+++ src/test/scala/com/epam/hadoopcourse/homework/spark/ApplicationTest.scala
@@ -1,8 +1,7 @@
 package com.epam.hadoopcourse.homework.spark
 
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
-import org.apache.spark.SparkConf
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.DataFrame
 import org.scalatest.FunSuite
 
 class ApplicationTest
@@ -11,11 +10,6 @@ class ApplicationTest
     with SparkSessionTestWrapper {
 
   test("Application.main") {
-
-    val conf: SparkConf = new SparkConf()
-      .setMaster("local")
-    val spark: SparkSession = SparkSession.builder().config(conf).getOrCreate()
-
     val sourceFile = "target/scala-2.11/test-classes/train.parquet"
     val targetFile = "target/scala-2.11/test-classes/task1.json"
     val expectedFile = "target/scala-2.11/test-classes/expected.json"
